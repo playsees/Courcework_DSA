3fd08a835326058349fd2f21f997f276a88270e3
playsees <124029605+playsees@users.noreply.github.com>
2024-01-20 16:55:22 +0700

    Update suffix_tree.c

--- suffix_tree.c
+++ suffix_tree.c
@@ -2,87 +2,101 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Node {
+typedef struct Node {
     char sub[100];
-    int ch[100];
-    int ch_size;
-};
+    int* ch;
+} Node;
 
-struct SuffixTree {
-    struct Node nodes[100];
-    int nodes_size;
-};
+typedef struct SuffixTree {
+    Node* nodes;
+} SuffixTree;
 
-void addSuffix(struct SuffixTree* tree, const char* suf) {
+Node* createNode(char* sub, int* children) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    strcpy(node->sub, sub);
+    node->ch = children;
+    return node;
+}
+
+SuffixTree* createSuffixTree(char* str) {
+    SuffixTree* suffixTree = (SuffixTree*)malloc(sizeof(SuffixTree));
+    suffixTree->nodes = (Node*)malloc(sizeof(Node));
+    suffixTree->nodes[0] = *createNode("", NULL);
+    for (int i = 0; i < strlen(str); i++) {
+        addSuffix(suffixTree, str + i);
+    }
+    return suffixTree;
+}
+
+void addSuffix(SuffixTree* suffixTree, char* suf) {
     int n = 0;
     int i = 0;
     while (i < strlen(suf)) {
         char b = suf[i];
         int x2 = 0;
-        int n2;
         while (1) {
-            int* children = tree->nodes[n].ch;
-            int children_size = tree->nodes[n].ch_size;
-            if (x2 == children_size) {
-                n2 = tree->nodes_size;
-                strcpy(tree->nodes[n2].sub, suf + i);
-                tree->nodes[n2].ch_size = 0;
-                children[children_size] = n2;
-                tree->nodes[n].ch_size++;
+            int* children = suffixTree->nodes[n].ch;
+            if (x2 == sizeof(children) / sizeof(children[0])) {
+                int n2 = sizeof(suffixTree->nodes) / sizeof(suffixTree->nodes[0]);
+                suffixTree->nodes = (Node*)realloc(suffixTree->nodes, (n2 + 1) * sizeof(Node));
+                suffixTree->nodes[n2] = *createNode(suf + i, NULL);
+                suffixTree->nodes[n].ch = (int*)realloc(suffixTree->nodes[n].ch, (x2 + 1) * sizeof(int));
+                suffixTree->nodes[n].ch[x2] = n2;
                 return;
             }
-            n2 = children[x2];
-            if (tree->nodes[n2].sub[0] == b) {
+            int n2 = children[x2];
+            if (suffixTree->nodes[n2].sub[0] == b) {
                 break;
             }
-            x2++;
+            x2 = x2 + 1;
         }
-        char* sub2 = tree->nodes[n2].sub;
+        char* sub2 = suffixTree->nodes[n2].sub;
         int j = 0;
         while (j < strlen(sub2)) {
             if (suf[i + j] != sub2[j]) {
                 int n3 = n2;
-                n2 = tree->nodes_size;
-                strncpy(tree->nodes[n2].sub, sub2, j);
-                tree->nodes[n2].sub[j] = '\0';
-                tree->nodes[n2].ch_size = 1;
-                tree->nodes[n2].ch[0] = n3;
-                tree->nodes[n3].sub[0] = '\0';
-                tree->nodes[n].ch[x2] = n2;
+                int n2 = sizeof(suffixTree->nodes) / sizeof(suffixTree->nodes[0]);
+                suffixTree->nodes = (Node*)realloc(suffixTree->nodes, (n2 + 1) * sizeof(Node));
+                suffixTree->nodes[n2] = *createNode(sub2, (int*)malloc(sizeof(int)));
+                strcpy(suffixTree->nodes[n2].sub, sub2 + j);
+                suffixTree->nodes[n].ch[x2] = n2;
                 break;
             }
-            j++;
+            j = j + 1;
         }
-        i += j;
+        i = i + j;
         n = n2;
     }
 }
 
-void visualize(struct SuffixTree* tree, int n, const char* pre) {
-    int* children = tree->nodes[n].ch;
-    int children_size = tree->nodes[n].ch_size;
-    if (children_size == 0) {
-        printf("- %s\n", tree->nodes[n].sub);
+void visualize(SuffixTree* suffixTree) {
+    if (sizeof(suffixTree->nodes) / sizeof(suffixTree->nodes[0]) == 0) {
+        printf("<empty>\n");
         return;
     }
-    printf("+ %s\n", tree->nodes[n].sub);
-    int i;
-    for (i = 0; i < children_size - 1; i++) {
-        printf("%s+-", pre);
-        visualize(tree, children[i], strcat(strdup(pre), "| "));
+    void f(int n, char* pre) {
+        int* children = suffixTree->nodes[n].ch;
+        if (sizeof(children) / sizeof(children[0]) == 0) {
+            printf("-- %s\n", suffixTree->nodes[n].sub);
+            return;
+        }
+        printf("+- %s\n", suffixTree->nodes[n].sub);
+        for (int i = 0; i < sizeof(children) / sizeof(children[0]) - 1; i++) {
+            printf("%s+- ", pre);
+            f(children[i], strcat(pre, " | "));
+        }
+        printf("%s+- ", pre);
+        f(children[sizeof(children) / sizeof(children[0]) - 1], strcat(pre, "  "));
     }
-    printf("%s+-", pre);
-    visualize(tree, children[children_size - 1], strcat(strdup(pre), "  "));
+    f(0, "");
 }
 
 int main() {
-    struct SuffixTree tree;
-    strcpy(tree.nodes[0].sub, "");
-    tree.nodes[0].ch_size = 0;
-    tree.nodes_size = 1;
-    addSuffix(&tree, "banana$");
-    visualize(&tree, 0, "");
+    SuffixTree* suffixTree = createSuffixTree("banana$");
+    visualize(suffixTree);
     return 0;
 }
+
+
 
 
